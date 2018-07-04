ce36e711350e3d0aa15ea73aead7688d89279b5d
Neyas Anand <neyasanand@gmail.com>
2018-07-04 22:22:08 +0530

    Added the create category

---
+++ src/test/java/com/neyas/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,69 @@
+package com.neyas.spring5webfluxrest.controllers;
+
+import com.neyas.spring5webfluxrest.domain.Category;
+import com.neyas.spring5webfluxrest.repository.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+
+        categoryController = new CategoryController(categoryRepository);
+
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void list() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("Cat1").build(),
+                        Category.builder().description("Cat2").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        BDDMockito.given(categoryRepository.findById(ArgumentMatchers.anyString()))
+                .willReturn(Mono.just(Category.builder().description("cat1").build()));
+
+        webTestClient.get().uri("/api/v1/categories/abcd")
+                .exchange()
+                .expectBody(Category.class);
+    }
+
+    @Test
+    public void testCreateCategory(){
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+        .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> categoryMono = Mono.just(Category.builder().description("Some Cat").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(categoryMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+}
